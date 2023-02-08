ee824c2ecbdb45bacef508e06ce9e73e1ea6cb6e
Rohan-1-3 <rohanshrestha130@gmail.com>
2023-02-08 19:27:36 +0545

    added webpack json

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    devtool: "inline-source-map",
+    path: path.resolve(__dirname, "project"),
+  },
+};
